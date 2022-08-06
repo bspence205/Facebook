7076f1c0805233d48bac66e42170f62f983db319
bspence205 <brianspencer@spencerbrian.com>
2022-08-06 02:41:51 -0700

    created HeaderIcon component and passed props through it for the header Icons, middle header component is complete with an active group in place to show over the home icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,10 +14,12 @@ import {
     SearchIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/outline"
+import HeaderIcon from './HeaderIcon'
 
 function Header() {
   return (
-    <div>
+    <div className='sticky top-0 z-50 bg-white flex items-center p-2
+    lg:px-5 shadow-md'>
         {/* left */}
         <div className='flex items-center'>
             <Image src="https://links.papareact.com/5me"
@@ -26,14 +28,27 @@ function Header() {
             <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                 <SearchIcon className='h-5 w-5 text-gray-600'/>
                 <input
-                className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' 
+                className='hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' 
                 type="text" placeholder='Search Facebook' name="" id="" />
             </div>
         </div>
 
         {/* center */}
-
+        <div className='flex justify-center flex-grow'>
+            <div className='flex space-x-6 md:space-x-2'>
+                <HeaderIcon active={true} Icon={HomeIcon}/>
+                <HeaderIcon active={false} Icon={FlagIcon}/>
+                <HeaderIcon active={false} Icon={PlayIcon}/>
+                <HeaderIcon active={false} Icon={UserGroupIcon}/>
+                <HeaderIcon active={false} Icon={ShoppingCartIcon}/>
+            </div>
+        </div>
         {/* right */}
+        <div className='flex items-center sm:space-x-2'>
+            {/* Profile Pic */}
+
+            <p className='font-semibold pr-3 whitespace-nowrap'>Brian Spencer</p>
+        </div>
     </div>
   )
 }
